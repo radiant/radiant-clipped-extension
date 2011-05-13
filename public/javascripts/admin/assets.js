e1a26977bf17a06dfda92e6c40eb34dc9f48387f
spanner <will@spanner.org>
2011-05-13 23:16:04 +0100

    no need to post-process new attachment html

--- public/javascripts/admin/assets.js
+++ public/javascripts/admin/assets.js
@@ -51,10 +51,19 @@ Asset.Upload = Behavior.create({
   }
 });
 
+Asset.GenerateUUID = function () {
+  // http://www.ietf.org/rfc/rfc4122.txt
+  var s = [];
+  var hexDigits = "0123456789ABCDEF";
+  for (var i = 0; i < 32; i++) { s[i] = hexDigits.substr(Math.floor(Math.random() * 0x10), 1); }
+  s[12] = "4";                                       // bits 12-15 of the time_hi_and_version field to 0010
+  s[16] = hexDigits.substr((s[16] & 0x3) | 0x8, 1);  // bits 6-7 of the clock_seq_hi_and_reserved to 01
+  return s.join('');
+};
+
 Asset.AddToList = function (html) {
   var list = $('attachment_fields');
-  var new_id  = new Date().getTime();
-  list.insert(html.replace(/new_attachment/g, new_id));
+  list.insert(html);
   if (list.hasClassName('empty')) {
     list.removeClassName('empty');
     list.slideDown();
