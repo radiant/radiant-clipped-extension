58101ee2568190617f0cde14960801cb2a2f8e75
Jim Gay <jim@saturnflyer.com>
2011-05-18 14:54:40 -0400

    fix document icon image

--- spec/models/asset_spec.rb
+++ spec/models/asset_spec.rb
@@ -63,7 +63,7 @@ describe Asset do
     
     it 'should return icon for non-image with a given size' do
       image = new_asset :asset_content_type => 'application/msword'
-      image.thumbnail('thumbnail').should == "/images/assets/other_thumbnail.png"
+      image.thumbnail('thumbnail').should == "/images/admin/assets/document_icon.png"
     end
   end
 
