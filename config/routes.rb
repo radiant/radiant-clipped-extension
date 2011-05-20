e98fde32407505178125d8440650d1b8e937ba68
spanner <will@spanner.org>
2011-05-20 10:29:58 +0100

    fixed refresh thumbnails link

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
   map.namespace :admin, :member => { :remove => :get } do |admin|
-    admin.resources :assets, :collection => { :refresh => :post }
+    admin.resources :assets, :collection => { :refresh => :get }
     admin.resources :page_attachments, :only => [:new]
     admin.resources :pages, :has_many => :page_attachments
   end
