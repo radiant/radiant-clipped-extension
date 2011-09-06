e6badece272c8ad05584a13b6fe13ee166a4c3fc
jsntv200 <jsntv200@gmail.com>
2011-09-06 14:32:47 +1000

    bump the version so bundler can find it

--- lib/radiant-clipped-extension.rb
+++ lib/radiant-clipped-extension.rb
@@ -1,8 +1,8 @@
 module RadiantClippedExtension
-  VERSION = '1.0.4'
-  SUMMARY = %q{Assets for Radiant CMS}
+  VERSION     = "1.0.6"
+  SUMMARY     = %q{Assets for Radiant CMS}
   DESCRIPTION = %q{Asset-management derived from Keith Bingman's Paperclipped extension.}
-  URL = "http://radiantcms.org"
-  AUTHORS = ["Keith Bingman", "Benny Degezelle", "William Ross", "John W. Long"]
-  EMAIL = ["radiant@radiantcms.org"]
+  URL         = "http://radiantcms.org"
+  AUTHORS     = ["Keith Bingman", "Benny Degezelle", "William Ross", "John W. Long"]
+  EMAIL       = ["radiant@radiantcms.org"]
 end
