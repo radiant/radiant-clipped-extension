7912572fcbf0a880f2dbe7691d0e1af074601b80
john muhl <git@johnmuhl.com>
2011-10-22 13:56:02 -0500

    change require to work on 1.9.2 and 1.8.7

--- spec/lib/geometry_transformation_spec.rb
+++ spec/lib/geometry_transformation_spec.rb
@@ -1,4 +1,4 @@
-require File.dirname(__FILE__) + '/../spec_helper'
+require File.expand_path('../../spec_helper', __FILE__)
 
 describe Paperclip::Geometry do
   let(:original) { Paperclip::Geometry.new(1200,600) }
