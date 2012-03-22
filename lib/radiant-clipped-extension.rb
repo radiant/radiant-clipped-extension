9b675265fd9cf9d6d8ff78b60ce0d6e6dc9dedee
Jim Gay <jim@saturnflyer.com>
2012-03-22 15:31:11 -0400

    bumped to a new minor version because paperclip is a new minor version

--- lib/radiant-clipped-extension.rb
+++ lib/radiant-clipped-extension.rb
@@ -1,5 +1,5 @@
 module RadiantClippedExtension
-  VERSION     = "1.0.17"
+  VERSION     = "1.1.0"
   SUMMARY     = %q{Assets for Radiant CMS}
   DESCRIPTION = %q{Asset-management derived from Keith Bingman's Paperclipped extension.}
   URL         = "http://radiantcms.org"
