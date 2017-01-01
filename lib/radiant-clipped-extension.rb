5b0d83e39c86d550c3d66890ea5eff25356882b1
Andrew vonderLuft <avonderluft@avlux.net>
2016-12-31 19:55:44 -0800

    bump version

--- lib/radiant-clipped-extension.rb
+++ lib/radiant-clipped-extension.rb
@@ -1,8 +1,8 @@
 module RadiantClippedExtension
-  VERSION     = "1.1.2"
+  VERSION     = "1.1.3"
   SUMMARY     = %q{Assets for Radiant CMS}
   DESCRIPTION = %q{Asset-management derived from Keith Bingman's Paperclipped extension.}
   URL         = "http://radiantcms.org"
-  AUTHORS     = ["Keith Bingman", "Benny Degezelle", "William Ross", "John W. Long", "John Muhl"]
+  AUTHORS     = ["Keith Bingman", "Benny Degezelle", "William Ross", "John W. Long", "John Muhl", "Andrew vonderLuft"]
   EMAIL       = ["radiant@radiantcms.org"]
 end
